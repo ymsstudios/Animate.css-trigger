66b7e4a92d651d19dcb34f4ba5b851e5c6099b60
Martin Schwartz <ymsstudios@gmail.com>
2016-09-29 23:59:29 -0400

    Update trigger.js

--- trigger.js
+++ trigger.js
@@ -1,3 +1,11 @@
+/*!
+ * Animate.css viewport trigger
+ * Version - 1.0.0
+ * Licensed under the MIT license - http://opensource.org/licenses/MIT
+ *
+ * Copyright (c) 2016 Martin Schwartz
+ */
+
 $(window).scroll(function () {
     $(".animated").each(function(){
         if ($(window).scrollTop() + $(window).height() > $(this).offset().top) {
